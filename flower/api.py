f12f83e2513acd468cec411c6c796115d069e8cb
hereariim <herearii.metuarea@gmail.com>
2022-07-10 16:32:26 +0200

    fr

--- flower/api.py
+++ flower/api.py
@@ -2,16 +2,16 @@ from functools import wraps
 import shutil
 import tempfile
 
-from aiohttp.web import HTTPBadRequest
-from webargs import files, validate
+from aiohttp import web #HTTPBadRequest
+from webargs import fields, validate
 
 def _catch_error(f):
     @wraps(f)
-    def wrap(*args,**kwargs)
+    def wrap(*args,**kwargs):
         try:
             return f(*args,**kwargs)
         except Exception as e:
-            raise HTTPBadRequest(reason=e)
+            raise web.HTTPBadRequest(reason=e)
     return wrap
 
 def get_metadata():
@@ -24,21 +24,25 @@ def get_metadata():
 
 def get_predict_args():
     """
-    INPUT
+    Input fields for the user.
     """
     arg_dict = {
-        "demo-image": fields.Field(
+        "image": fields.Field(
             required=False,
-            type="file";
-            location="form";
-            description="image",
+            type="file",
+            missing="None",
+            location="form",
+            description="image",  # needed to be parsed by UI
         ),
+
         "accept": fields.Str(
-            description="Media is ok",
-            validate=validate.OneOf(["image/*","application/zip"])
-        )
+            description="Media type(s) that is/are acceptable for the response.",
+            missing='application/zip',
+            validate=validate.OneOf(['application/zip', 'image/png', 'application/json']),
+        ),
     }
     return arg_dict
+
 
 @_catch_error
 def predict(**kwargs):
@@ -46,10 +50,32 @@ def predict(**kwargs):
     OUTPUT
     """
 
-    filepath = kwargs["demo-image"].filename
+    filepath = kwargs["image"].filename
     
     # Return the image directly
-    if kwargs['accept'] == 'image/*':
+    if kwargs['accept'] == 'image/png':
         return open(filepath, 'rb')
+    
+    # Return a zip
+    elif kwargs['accept'] == 'application/zip':
 
+        zip_dir = tempfile.TemporaryDirectory()
+
+        # Add original image to output zip
+        shutil.copyfile(filepath,
+                        zip_dir.name + '/demo.png')
+
+        # Add for example a demo txt file
+        with open(f'{zip_dir.name}/demo.txt', 'w') as f:
+            f.write('Add here any additional information!')
+
+        # Pack dir into zip and return it
+        shutil.make_archive(
+            zip_dir.name,
+            format='zip',
+            root_dir=zip_dir.name,
+        )
+        zip_path = zip_dir.name + '.zip'
+
+        return open(zip_path, 'rb')
     
